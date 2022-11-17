96a3f0b2762f46a55f508470cae7bb03acb0077b
salinpaul94 <salinpv@hotmail.com>
2022-11-18 02:58:01 +0530

    fetching lyrics and displaying

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode } from "swiper";
+import { FreeMode } from 'swiper';
 
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
@@ -12,41 +12,41 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 
 const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick,
-handlePlayClick}) => (
-  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2
-  p-4 rounded-lg cursor-pointer mb-2"
-  >
-    <h3 className="fot-bold text-base text-white mr-3">
-      {i + 1}
-    </h3>
-    <div className="flex-1 flex flex-row justify-between items-center">
-      <img
-        className="w-20 h-20 rounded-lg"
-        src={song?.images?.coverart}
-        alt={song?.title}
-      />
-      <div className="flex-1 flex flex-col justify-center mx-3">
-        <Link to={`/songs/${song.key}`}>
-          <p className="text-xl font-bold text-white">
-            {song?.title}
-          </p>
-        </Link>
-        <Link to={`/artists/${song?.artists[0].adamid}`}>
-          <p className="text-base text-gray-300 mt-1">
-            {song?.subtitle}
-          </p>
-        </Link>
+  handlePlayClick }) => (
+    <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2
+    p-4 rounded-lg cursor-pointer mb-2"
+    >
+      <h3 className="font-bold text-base text-white mr-3">
+        {i + 1}.
+      </h3>
+      <div className="flex-1 flex flex-row justify-between items-center">
+        <img
+          className="w-20 h-20 rounded-lg"
+          src={song?.images?.coverart}
+          alt={song?.title}
+        />
+        <div className="flex-1 flex flex-col justify-center mx-3">
+          <Link to={`/songs/${song.key}`}>
+            <p className="text-xl font-bold text-white">
+              {song?.title}
+            </p>
+          </Link>
+          <Link to={`/artists/${song?.artists[0].adamid}`}>
+            <p className="text-base text-gray-300 mt-1">
+              {song?.subtitle}
+            </p>
+          </Link>
+        </div>
       </div>
+      <PlayPause
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        song={song}
+        handlePause={handlePauseClick}
+        handlePlay={handlePlayClick}
+      />
     </div>
-    <PlayPause
-      isPlaying={isPlaying}
-      activeSong={activeSong}
-      song={song}
-      handlePause={handlePauseClick}
-      handlePlay={handlePlayClick}
-    />
-  </div>
-)
+);
 
 const TopPlay = () => {
   const dispatch = useDispatch();
@@ -118,7 +118,7 @@ const TopPlay = () => {
             <SwiperSlide
               key={song?.key}
               style={{ width: '25%', height: 'auto'}}
-              className="shadow-lg rounded-full animate-slider"
+              className="shadow-lg rounded-full animate-slideright"
             >
               <Link to={`/artists/${song?.artists[0].adamid}`}>
                 <img
